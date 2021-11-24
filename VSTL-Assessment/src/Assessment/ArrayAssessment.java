d4402575287962f671331f4f917bd52eca46462d
admin <admin@admin-PC>
2021-11-24 20:26:53 +0530

    ArrayAssessment

--- VSTL-Assessment/src/Assessment/ArrayAssessment.java
+++ VSTL-Assessment/src/Assessment/ArrayAssessment.java
@@ -31,30 +31,49 @@ public class ArrayAssessment {
 			System.out.println(age[all]);
 		}
 
-	    // double array :
-		
-		// height of Trainees 
-		
-		double height[] = new double[3];
-		
-		height[0]= 170.2;
-		height[1]= 165.5;
-		height[2]= 172.8;
-		
-		System.out.println("Preeti "+height[0]);
-		System.out.println("Divya "+height[1]);
+		// double array :
 
-				
-		//boolean array :
-		
-		boolean statement[]= new boolean[3];
-		
-		statement[0]= true;
-		statement[1]= false;
-		
-		System.out.println("Got the output: "+statement[0]);
-		
-				
+		// height of Trainees
+
+		double height[] = new double[3];
+
+		height[0] = 170.2;
+		height[1] = 165.5;
+		height[2] = 172.8;
+
+		System.out.println("Preeti " + height[0]);
+		System.out.println("Divya " + height[1]);
+
+		// boolean array :
+
+		boolean statement[] = new boolean[3];
+
+		statement[0] = true;
+		statement[1] = false;
+
+		System.out.println("Got the output: " + statement[0]);
+
+		// char array
+
+		char sym[] = new char[2];
+
+		sym[0] = '$';
+		sym[1] = '#';
+
+		System.out.println(sym[0]);
+
+		// String array
+
+		String name[] = new String[3];
+
+		name[0] = "Hello";
+		name[1] = "Yogesh";
+		name[2] = "Sir";
+
+		for (int gesture = 0; gesture < name.length; gesture++) {
+			System.out.println(name[gesture]);
+
+		}
 
 	}
 
